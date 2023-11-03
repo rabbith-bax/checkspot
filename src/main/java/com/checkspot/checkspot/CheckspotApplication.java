218d17113a37e8da3010b0afb530538cf61ea088
oscarzajac <oscar.zajac@efecte.com>
2023-11-03 13:59:14 +0100

    initial commit with first idea of needed dependencies

---
+++ src/main/java/com/checkspot/checkspot/CheckspotApplication.java
@@ -0,0 +1,13 @@
+package com.checkspot.checkspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkspot/checkspot/CheckspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkspot.checkspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
