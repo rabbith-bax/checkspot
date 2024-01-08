b2315fb60d8d85fdd31fc2ea53ab0241f337bba6
oscarzajac <oscar.zajac@efecte.com>
2024-01-08 12:46:52 +0100

    added table name for users

--- src/main/java/com/checkspot/checkspot/model/User.java
+++ src/main/java/com/checkspot/checkspot/model/User.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "Accounts")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
